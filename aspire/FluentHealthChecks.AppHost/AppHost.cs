67a3b9b02de3a923baf32a55aba1723224aa9a1f
thomson-jayden <thomson.jayden@outlook.com>
2026-04-22 21:03:45 +1000

    have the health endpoint clickable in the aspire dashboard

--- aspire/FluentHealthChecks.AppHost/AppHost.cs
+++ aspire/FluentHealthChecks.AppHost/AppHost.cs
@@ -2,6 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 _ = builder
     .AddProject<Projects.FluentHealthChecks_Example_Api>("api")
-    .WithExternalHttpEndpoints();
+    .WithExternalHttpEndpoints()
+    .WithHttpHealthCheck("/health")
+    .WithUrl("/health", "Health");
 
 builder.Build().Run();
